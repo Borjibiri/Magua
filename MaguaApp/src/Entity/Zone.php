74084549031f07f3f18a437b250fdaccdeb3eb00
Borjibiri <bosanper@gmail.com>
2024-11-12 14:04:53 +0000

    init commit

---
+++ MaguaApp/src/Entity/Zone.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ZoneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ZoneRepository::class)]
+class Zone
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city_name = null;
+
+    #[ORM\Column]
+    private ?int $zip_code = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCityName(): ?string
+    {
+        return $this->city_name;
+    }
+
+    public function setCityName(string $city_name): static
+    {
+        $this->city_name = $city_name;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?int
+    {
+        return $this->zip_code;
+    }
+
+    public function setZipCode(int $zip_code): static
+    {
+        $this->zip_code = $zip_code;
+
+        return $this;
+    }
+}
