2bdd2934839549a3c0079cf633123c1473881034
Borjibiri <bosanper@gmail.com>
2024-11-22 00:25:19 +0000

    changed order in user dashboard.

--- MaguaApp/src/Form/RegistrationFormType.php
+++ MaguaApp/src/Form/RegistrationFormType.php
@@ -23,7 +23,7 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add("address")            
             ->add('agreeTerms', CheckboxType::class, [
-                                'mapped' => false,
+                'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
